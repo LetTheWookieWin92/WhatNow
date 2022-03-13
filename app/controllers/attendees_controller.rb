0e5ca2f8d55608ab28b01716d20d2e0dbbe29986
Geoff Reid <geoff_reid@outlook.com>
2022-03-13 16:58:59 +1100

    User is now able to remove events they've registered for, with a safety prompt

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -12,4 +12,14 @@ class AttendeesController < ApplicationController
         redirect_to root_path
     end
 
+    def destroy
+
+        @event = Event.find(params[:id])
+        @event.event_registrations.delete(current_user)
+        flash[:notice] = "You have been removed from this event"
+
+        redirect_to root_path
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
     # Route for user profile
     get 'profile/:id', to: 'profile#display', as: "profile"
 
+    # Route for when users want to add themselves to an event (add attendee)
     post '/users/:user_id/events/:id', to: "attendees#create", as: "create_attendee"
+
     get '/profile/:id/event_attendances', to: "profile#show_event_attendances", as: "show_event_attendances"
 
+    # Route for when users want to remove themselves from an event (remove attendee)
+    delete '/users/:user_id/events/:id', to: "attendees#destroy", as: "destroy_attendee"
 end
