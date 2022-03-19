5e527f32eaf43fb54310d1ac435a280179d8bc01
Geoff Reid <geoff_reid@outlook.com>
2022-03-19 14:00:03 +1100

    Commit prior to clean up

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
     # Users can be the organiser of events
     belongs_to :organiser, class_name: "User"
 
-    has_many :attendees
+    has_many :attendees, dependent: :delete_all
     has_many :event_registrations, through: :attendees, source: :user
     has_one_attached :picture
 
